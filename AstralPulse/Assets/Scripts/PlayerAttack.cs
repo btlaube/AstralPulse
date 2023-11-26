dacafb809bb38921d480385a06c3b4303ed0ea67
btlaube <ben.t.laube@gmail.com>
2023-11-25 23:33:37 -0600

    Added visuals
    attack twinkle and pulse

--- AstralPulse/Assets/Scripts/PlayerAttack.cs
+++ AstralPulse/Assets/Scripts/PlayerAttack.cs
@@ -6,13 +6,20 @@ public class PlayerAttack : MonoBehaviour
 {
 
     // Animator
-    private Animator animator;
+    private Animator playerAnimator;
 
     [SerializeField] private int attackLevel;
     [SerializeField] private int maxAttackLevel;
     [SerializeField] private int currentAttackLevel;
     
-    //Attack charge 
+    [Header("Attack Visual")]
+    // public GameObject twinklePrefab;
+    private Transform twinkleTransform;
+    private Transform pulseTransform;
+    private GameObject twinkle;
+    private GameObject pulse;
+
+    // Attack charge 
     [SerializeField] private float attackChargeRate;
     [SerializeField] private float attackChargeTimer;
 
@@ -22,7 +29,14 @@ public class PlayerAttack : MonoBehaviour
 
     void Awake()
     {
-        animator = GetComponentInChildren<Animator>();
+        playerAnimator = transform.Find("PlayerVisual").gameObject.GetComponentInChildren<Animator>();
+        
+        twinkleTransform = transform.Find("AttackVisual/AttackTwinkle");
+        twinkle = twinkleTransform.gameObject;
+
+        pulseTransform = transform.Find("AttackVisual/AttackPulse");
+        pulse = pulseTransform.gameObject;
+
         particleSystem = GetComponentInChildren<ParticleSystem>();
     }
 
@@ -31,7 +45,9 @@ public class PlayerAttack : MonoBehaviour
     {
         // particleSystem.Play();
         currentAttackLevel = 1;
-        animator.SetInteger("AttackLevel", -1);
+        playerAnimator.SetInteger("AttackLevel", -1);
+        twinkle.SetActive(false);
+        pulse.SetActive(false);
     }
 
     // Update is called once per frame
@@ -59,26 +75,29 @@ public class PlayerAttack : MonoBehaviour
             {
                 attackChargeTimer = 0;
                 currentAttackLevel += 1;
-                // animator.SetInteger("AttackLevel", currentAttackLevel);
+                // playerAnimator.SetInteger("AttackLevel", currentAttackLevel);
             }
-            animator.SetInteger("AttackLevel", currentAttackLevel);
+            playerAnimator.SetInteger("AttackLevel", currentAttackLevel);
+            
+
             
             switch (currentAttackLevel)
             {
                 case 1:
-                    // SetParticleSystem(0.44f, -0.9f, 0.07f, 5.0f, 0.5f);
                     SetParticleSystem(0.4f, -1.3f, 0.10f, 30.0f, 0.65f);
+                    twinkleTransform.localScale = Vector3.one * 0.5f;
+                    pulseTransform.localScale = Vector3.one * 0.7f;
                     break;
                 case 2:
-                    // SetParticleSystem(0.4f, -1.3f, 0.10f, 30.0f, 0.65f);
                     SetParticleSystem(0.4f, -1.45f, 0.25f, 50.0f, 0.7f);
+                    twinkleTransform.localScale = Vector3.one * 0.8f;
+                    pulseTransform.localScale = Vector3.one * 1.0f;
                     break;
                 case 3:
                     SetParticleSystem(0.4f, -1.45f, 0.25f, 50.0f, 0.7f);
+                    twinkleTransform.localScale = Vector3.one * 1.0f;
+                    pulseTransform.localScale = Vector3.one * 1.5f;
                     break;
-                // default:
-                //     particleSystem.Stop();
-                //     break;
             }
             if (!isEmitting)
             {
@@ -91,14 +110,27 @@ public class PlayerAttack : MonoBehaviour
 
     private void ReleaseAttack()
     {
-        Debug.Log($"Released attack level {currentAttackLevel}");
+        // Debug.Log($"Released attack level {currentAttackLevel}");
         attackChargeTimer = 0;
         currentAttackLevel = 1;
-        animator.SetInteger("AttackLevel", 0);
-        animator.SetTrigger("ReleaseAttack");
+        playerAnimator.SetInteger("AttackLevel", 0);
+        playerAnimator.SetTrigger("ReleaseAttack");
+        
+        StopAllCoroutines();
+        StartCoroutine("TwinkleAnimation");
+
         particleSystem.Stop();
         particleSystem.Clear();
         isEmitting = false;
+    }
+
+    private IEnumerator TwinkleAnimation()
+    {
+        twinkle.SetActive(true);
+        pulse.SetActive(true);
+        yield return new WaitForSeconds(0.7f);
+        twinkle.SetActive(false);
+        pulse.SetActive(false);
     }
 
     private void SetParticleSystem(float startLifetime, float startSpeed, float startSize, float rateOverTime, float radius)
